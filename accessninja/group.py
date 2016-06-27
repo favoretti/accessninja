a78df7da3b22f26179c84c9dc3ad2eaea2e09112
Vladimir Lazarenko <vlazarenko@ebay.com>
2016-06-27 10:07:47 +0200

    More fiddlings

--- accessninja/group.py
+++ accessninja/group.py
@@ -14,6 +14,7 @@ class HostGroup(object):
         try:
             f = open('{}/{}.hosts'.format(self.config.objects, groupname))
             lines = f.readlines()
+            self._name = groupname
 
             for line in lines:
                 if line.startswith('#'):
@@ -24,17 +25,47 @@ class HostGroup(object):
                     continue
                 if line.strip():
                     prefix = line.split('#')[0].strip()
-                    print prefix
+                    self._prefixes.append(prefix)
         except Exception, e:
             print e
+
+    def hosts(self):
+        return self._prefixes
 
 class PortGroup(object):
 
     def __init__(self):
+        self.config = Config()
         self._name = None
         self._ports = list()
 
+    def parseFile(self, groupname):
+        try:
+            f = open('{}/{}.ports'.format(self.config.objects, groupname))
+            lines = f.readlines()
+            self._name = groupname
+
+            for line in lines:
+                if line.startswith('#'):
+                    continue
+                if line.startswith('@'):
+                    rec_group = line.split(' ')[0][1:].replace('\n', '')
+                    self.parseFile(rec_group)
+                    continue
+                if line.strip():
+                    port = line.split('#')[0].strip()
+                    self._ports.append(port)
+        except Exception, e:
+            print e
+
+
+    def ports(self):
+        return self._ports
 
 if __name__ == '__main__':
     hg = HostGroup()
-    hg.parseFile(sys.argv[1])
+    hg.parseFile('qa')
+    print hg.hosts()
+    pg = PortGroup()
+    pg.parseFile('hadoop_mn_services')
+    print pg.ports()
