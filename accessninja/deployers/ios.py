464b0ab7e13013fc4254005515ccce0b7b2ba1ff
Vladimir Lazarenko <favoretti@gmail.com>
2016-07-05 18:12:39 +0000

    Now added all of it ;)

---
+++ accessninja/deployers/ios.py
@@ -0,0 +1,35 @@
+import netrc
+import sys
+import time
+from tempfile import NamedTemporaryFile
+from netmiko import ConnectHandler
+
+
+class IOSDeployer(object):
+    def __init__(self, device):
+        self._device = device
+
+    def render_to_file_and_deploy(self):
+        username, acc, password = \
+            netrc.netrc().authenticators(self._device.name)
+
+        device_data = {
+            'device_type': 'cisco_ios',
+            'ip': self._device.name,
+            'username': username,
+            'password': password,
+        }
+
+        f = NamedTemporaryFile(delete=False)
+        print('Stored temporary config at {}'.format(f.name))
+        f.write(self._device.rendered_config)
+        f.flush()
+
+        net_connect = ConnectHandler(**device_data)
+        output = net_connect.send_config_from_file(f.name)
+        print(output)
+        print('Config uploaded!')
+
+        f.close()
+
+        print('Done.')
